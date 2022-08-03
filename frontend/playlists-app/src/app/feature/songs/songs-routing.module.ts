f9aadcb2fdc2c3106e2f14d0cfa218c24ab92296
Jeffry Gonzalez <jeff@hypertheory.com>
2022-08-03 12:23:56 -0400

    sketched out songs

--- frontend/playlists-app/src/app/feature/songs/songs-routing.module.ts
+++ frontend/playlists-app/src/app/feature/songs/songs-routing.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ListComponent } from "./components/list/list.component";
+import { NewComponent } from "./components/new/new.component";
+import { OverviewComponent } from "./components/overview/overview.component";
 import { SongsComponent } from "./songs.component";
 
 
 const routes: Routes = [
   {
     path: 'songs',
-    component: SongsComponent
+    component: SongsComponent,
+    children: [
+      {
+        path: 'overview',
+        component: OverviewComponent
+      },
+      {
+        path: 'list',
+        component: ListComponent
+      },
+      {
+        path: 'new',
+        component: NewComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'overview'
+      }
+    ]
   }
 ]
 
--- frontend/playlists-app/src/app/feature/songs/songs.module.ts
+++ frontend/playlists-app/src/app/feature/songs/songs.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SongsComponent } from './songs.component';
 import { SongsRoutingModule } from './songs-routing.module';
+import { OverviewComponent } from './components/overview/overview.component';
+import { ListComponent } from './components/list/list.component';
+import { NewComponent } from './components/new/new.component';
 
 
 
 @NgModule({
   declarations: [
-    SongsComponent
+    SongsComponent,
+    OverviewComponent,
+    ListComponent,
+    NewComponent
   ],
   imports: [
     SongsRoutingModule,
